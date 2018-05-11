1c061ab4278c5ce0d47e47762e444dfcf5b71993
rubensrocha <rubens.dragonforce@gmail.com>
2018-05-10 22:15:51 -0300

    Update languages

--- admin/inc/_menu_left.php
+++ admin/inc/_menu_left.php
@@ -13,27 +13,27 @@
     </div>
     <span class="heading"><?php echo $lang['menu_left']['_home'];?></span>
     <ul class="list-unstyled">
-        <li class="<?php echo (!$_GET['menu'])?'active':''; ?>"><a href="<?php $func->urlAdmin('');?>"><i class="fa fa-home"></i> Home</a></li>
-        <li <?php $func->activeMenu('users');?>><a href="<?php $func->urlAdmin('users');?>"><i class="fa fa-users"></i> View Users</a></li>
-        <li <?php $func->activeMenu('sender');?>><a href="<?php $func->urlAdmin('sender');?>"><i class="fa fa-send"></i> Mass mailing</a></li>
-        <li <?php $func->activeMenu('config');?>><a href="<?php $func->urlAdmin('config');?>"><i class="fa fa-cogs"></i> Settings</a></li>
+        <li class="<?php echo (!$_GET['menu'])?'active':''; ?>"><a href="<?php $func->urlAdmin('');?>"><i class="fa fa-home"></i> <?php echo $lang['menu_left']['home'];?></a></li>
+        <li <?php $func->activeMenu('users');?>><a href="<?php $func->urlAdmin('users');?>"><i class="fa fa-users"></i> <?php echo $lang['menu_left']['users'];?></a></li>
+        <li <?php $func->activeMenu('sender');?>><a href="<?php $func->urlAdmin('sender');?>"><i class="fa fa-send"></i> <?php echo $lang['menu_left']['sender'];?></a></li>
+        <li <?php $func->activeMenu('config');?>><a href="<?php $func->urlAdmin('config');?>"><i class="fa fa-cogs"></i> <?php echo $lang['menu_left']['settings'];?></a></li>
     </ul>
 
     <span class="heading"><?php echo $lang['menu_left']['_transactions'];?></span>
     <ul class="list-unstyled">
-        <li <?php $func->activeMenu('story_buy');?>><a href="<?php $func->urlAdmin('story_buy');?>"><i class="fa fa-bank"></i> Purchase history</a></li>
-        <li <?php $func->activeMenu('story_swap');?>><a href="<?php $func->urlAdmin('story_swap');?>"><i class="fa fa-refresh"></i> History of exchanges</a></li>
-        <li <?php $func->activeMenu('story_insert');?>><a href="<?php $func->urlAdmin('story_insert');?>"><i class="fa fa-arrow-circle-left"></i> History of replenishment</a></li>
-        <li <?php $func->activeMenu('story_sell');?>><a href="<?php $func->urlAdmin('story_sell');?>"><i class="fa fa-shopping-basket"></i> Sales in the market</a></li>
-        <li <?php $func->activeMenu('payments');?>><a href="<?php $func->urlAdmin('payments');?>"><i class="fa fa-money"></i> Payout list</a></li>
+        <li <?php $func->activeMenu('story_buy');?>><a href="<?php $func->urlAdmin('story_buy');?>"><i class="fa fa-bank"></i> <?php echo $lang['menu_left']['purchases'];?></a></li>
+        <li <?php $func->activeMenu('story_swap');?>><a href="<?php $func->urlAdmin('story_swap');?>"><i class="fa fa-refresh"></i> <?php echo $lang['menu_left']['exchanges'];?></a></li>
+        <li <?php $func->activeMenu('story_insert');?>><a href="<?php $func->urlAdmin('story_insert');?>"><i class="fa fa-arrow-circle-left"></i> <?php echo $lang['menu_left']['deposits'];?></a></li>
+        <li <?php $func->activeMenu('story_sell');?>><a href="<?php $func->urlAdmin('story_sell');?>"><i class="fa fa-shopping-basket"></i> <?php echo $lang['menu_left']['sales'];?></a></li>
+        <li <?php $func->activeMenu('payments');?>><a href="<?php $func->urlAdmin('payments');?>"><i class="fa fa-money"></i> <?php echo $lang['menu_left']['withdraws'];?></a></li>
     </ul>
 
     <span class="heading"><?php echo $lang['menu_left']['_sections'];?></span>
     <ul class="list-unstyled">
-        <li <?php $func->activeMenu('news');?>><a href="<?php $func->urlAdmin('news');?>"><i class="fa fa-newspaper-o"></i> News</a></li>
-        <li <?php $func->activeMenu('about');?>><a href="<?php $func->urlAdmin('about');?>"><i class="fa fa-exclamation-circle"></i> About</a></li>
-        <li <?php $func->activeMenu('rules');?>><a href="<?php $func->urlAdmin('rules');?>"><i class="fa fa-bookmark"></i> Rules</a></li>
-        <li <?php $func->activeMenu('contacts');?>><a href="<?php $func->urlAdmin('contacts');?>"><i class="fa fa-envelope"></i> Contact</a></li>
+        <li <?php $func->activeMenu('news');?>><a href="<?php $func->urlAdmin('news');?>"><i class="fa fa-newspaper-o"></i> <?php echo $lang['menu_left']['news'];?></a></li>
+        <li <?php $func->activeMenu('about');?>><a href="<?php $func->urlAdmin('about');?>"><i class="fa fa-exclamation-circle"></i> <?php echo $lang['menu_left']['about'];?></a></li>
+        <li <?php $func->activeMenu('rules');?>><a href="<?php $func->urlAdmin('rules');?>"><i class="fa fa-bookmark"></i> <?php echo $lang['menu_left']['rules'];?></a></li>
+        <li <?php $func->activeMenu('contacts');?>><a href="<?php $func->urlAdmin('contacts');?>"><i class="fa fa-envelope"></i> <?php echo $lang['menu_left']['contact'];?></a></li>
     </ul>
 
 </nav><!-- /Sidebar Header-->
--- langs/ru.php
+++ langs/ru.php
@@ -1,9 +1,279 @@
 <?php
 
 $lang = [
-    'langSelect' => 'язык',
+    'langSelect' => 'Languages',
+
     'menu_top' => [
         'home' => 'Home',
-        'news' => 'Новости',
+        'news' => 'News',
+        'contact' => 'Contact',
+        'account' => 'My Account',
+        'payments' => 'Payments',
+        'exit' => 'Logout',
+    ],
+
+    'menu_left' => [
+        '_home' => 'Home',
+        '_acc' => 'Account',
+        '_game' => 'Gameplay',
+        '_earn' => 'Earnings',
+
+        'login' => 'Login',
+        'register' => 'Register',
+        'resetpass' => 'Reset Password',
+        'rules' => 'Rules',
+        'about' => 'About Us',
+        'statistics' => 'Statistics',
+        'info' => 'Information',
+        'game' => 'Game',
+
+        'profit' => 'Profit Progress',
+        'p_balance' => 'Purchase',
+        'w_balance' => 'Balance',
+        'account' => 'My Account',
+        'referrals' => 'My Referrals',
+        'messages' => 'Messages',
+        'config' => 'Account Settings',
+
+        'games' => 'Games',
+        'bonus' => 'Bonus',
+        'surf' => 'View Ads',
+
+        'farm' => 'Store',
+        'store' => 'Collect Profit',
+        'market' => 'Trading Market',
+    ],
+
+    'error_messages' => [
+        'accountBanned' => 'Account is blocked',
+        'wrongLogin' => 'Email and / or Password is incorrect',
+        'notfoundAccount' => 'The specified Email is not registered in the system',
+        'captcha' => 'Captcha incorrect. Try again.',
+        'usernameInUse' => 'The specified login is already in use',
+        'passwordMatch' => 'Password and password repeat do not match',
+        'emptyPassword' => 'Password is incorrect or empty',
+        'emptyLogin' => 'Login incorrect or empty',
+        'emailInvalid' => 'Email has an invalid format',
+        'tosConfirm' => 'You did not confirm the rules',
+        'invalidData' => 'There were errors with the data you provided',
+        'requestReset' => 'Your email or IP has already been sent a password in the last 15 minutes',
+        'oldpassword' => 'Old password is incorrect',
+        'bonusCollected' => 'You have already received a bonus in the last 24 hours.',
+        'noresults' => 'No results to show',
+        'XXXXXXXXX' => 'XXXXXXX',
+    ],
+
+    'success_messages' => [
+        'changesSaved' => 'Changes saved with success!',
+        'bonusAdded' => 'A bonus was added to your shopping account in the amount of %s %s',
+        'XXXXXXXXX' => 'XXXXXXX',
+    ],
+
+    'btn' => [
+        'send' => 'Send',
+        'edit' => 'Edit',
+        'delete' => 'Delete',
+        'create' => 'Create',
+        'cancel' => 'Cancel',
+        'login' => 'Login',
+        'register' => 'Register',
+        'forgotL' => 'Reset Password',
+        'forgot' => 'Send Reset Link',
+        'deposit' => 'Deposit',
+        'withdraw' => 'Withdraw',
+        'exchange' => 'Exchange',
+        'save' => 'Save',
+        'getBonus' => 'Get Bonus',
+        'buyNow' => 'Buy Now',
+        'payNow' => 'Pay Now',
+        'sellAll' => 'Sell All',
+    ],
+
+    'common' => [
+        'email' => 'Email',
+        'password' => 'Password',
+        'password_old' => 'Old Password',
+        'password_new' => 'New Password',
+        'captcha' => 'Enter Captcha',
+        'captcha_h' => 'Click on image to generate a new captcha',
+        'auth' => 'Authorization',
+        'users' => 'Users',
+        'days' => 'Days',
+        'username' => 'Username',
+        'copyright' => 'All rights reserved.',
+        'referrer' => 'Inveted by',
+        'p_balance' => 'Puchase Balance',
+        'w_balance' => 'Withdraw Balance',
+        'ref_earnings' => 'Earning from Referrals',
+        'paid' => 'Total Paid',
+        'resgetedOn' => 'Registration Date',
+        'deposits' => 'Deposits',
+        'withdraws' => 'Withdraws',
+        'referrals' => 'Referrals',
+        'date' => 'Date',
+        'wallet' => 'Wallet',
+        'amount' => 'Amount',
+        'status' => 'Status',
+        'status_paid' => 'Paid',
+        'min' => 'Min.',
+        'conversionRate' => 'Conversion Rate',
+    ],
+
+    'home' => [
+        'title' => 'Welcome',
+        'total_users' => 'Total Users',
+        'working_time' => 'Working Time',
+        'total_paid' => 'Total Paid',
+    ],
+
+    'news' => [
+        'title' => 'News',
+        'noNews' => 'No news found',
+    ],
+
+    'about' => [
+        'title' => 'About',
+    ],
+
+    'rules' => [
+        'title' => 'Rules',
+    ],
+
+    'statistics' => [
+        'title' => 'Statistics',
+        'subtitle' => 'Project Statistics',
+        'totalUsers' => 'Players',
+        'users24' => '24 hours',
+        'projectStart' => 'Runing Days',
+        'deposits' => 'Replenished',
+        'reserve' => 'Project Reserve',
+        'withdraws' => 'Paid',
+    ],
+
+    'contact' => [
+        'title' => 'Contact Us',
+    ],
+
+    'register' => [
+        'title' => 'Register',
+        'username_h' => 'The username field must have from 4 to 20 alpha numeric characters (only English characters).',
+        'password_h' => 'The password field must have from 4 to 20 alpha numeric characters (only English characters).',
+        'passConfirm' => 'Password Confirm',
+        'passConfirm_h' => 'Passwords must match.',
+        'acceptRules' => 'I am familiar with the project rules and accept it.',
+    ],
+
+    'recovery' => [
+        'title' => 'Password Recovery',
+        'requestSended' => 'Reset link was sent to Email',
+        'resetSuccess' => 'Your password was changed with success, you can login into your account now.',
+    ],
+
+    'fail' => [
+        'title' => 'Deposit Failed',
+        'text' => 'Your deposit can not be confirmed. If the problem persists, contact the administrator immediately.',
+    ],
+
+    'success' => [
+        'title' => 'Deposit Confirmed',
+        'text' => 'Your deposit was confirmed with success. The amount deposited already available on your account.',
+    ],
+
+    'account' => [
+        'title' => 'My Account',
+        'details' => 'Account Details',
+        'statistics' => 'Account Statistics',
+        'chart' => 'Statistics Chart',
+    ],
+
+    'referrals' => [
+        'title' => 'My Referrals',
+        'refs_total' => 'Total of Referrals',
+        'ref_revenue' => 'Revenue From Referral',
+        'noreferrals' => 'You do not have any referral yet',
+    ],
+
+    'acc_settings' => [
+        'title' => 'Account Settings',
+        '_password' => 'Change Password',
+        'old_password' => 'Enter actual password to confirm the changes',
+        'updated' => 'Chan',
+    ],
+
+    'bonus_daily' => [
+        'title' => 'Daily Bonus',
+        'desc' => 'How it Works',
+        'rule1' => 'The bonus will be issued once in 24 hours.',
+        'rule2' => 'The bonus is given to Purchase Balance.',
+        'rule3' => 'The bonus amount is generated randomly from %s to %s %s.',
+        'leftTime' => 'Please wait: ',
+    ],
+
+    'payments' => [
+        'title' => 'Recent Payouts',
+        'subtitle' => 'Payments from last 48 hours',
+        'count' => 'Payments Total',
+        'total' => 'Amount Total',
+    ],
+
+    'store' => [
+        'title' => 'Warehouse',
+        'subtitle' => 'Payments from last 48 hours',
+        'count' => 'Payments Total',
+        'total' => 'Amount Total',
+        'success' => '%s collected with success',
+        'error' => 'You can not collected no more than 1 times in 10 minutesl',
+    ],
+
+    'farm' => [
+        'title' => 'Car Shop',
+        'text' => 'In this store you can buy new cars. Each car brings kilometers that can then be sold on the market and exchanged for real money. Each car gives a different number of kilometers, the more expensive it cost, more kilometers it generate. You can buy any number of them, the cars have not expiration date, it will always generate kilometers in your account.',
+        'alert' => 'Before buy, you should collect your %s in the warehouse!',
+        'purchased' => '%s purchased with success!',
+        'noEnoughCoins' => 'There is not enough %s to buy this item!',
+        'perHour' => 'Per Hour',
+        'perMonth' => 'Per Month',
+        'warehouse' => 'Your Warehouse',
+    ],
+
+    'market' => [
+        'title' => 'Trading Shop',
+        'text' => 'A retail store will allow you to sell all your %s, which can be exchanged for real money. The %s gained from the sale is distributed between two balances (Balance and Purchase Balance) in the proportion of %s%% to the Purchase Balance and %s%% to the Balance.',
+        'error' => 'You have nothing to sell',
+        'sold' => 'You sold %s %s, for the amount of %s %s',
+    ],
+
+    'swap' => [
+        'title' => 'Exchange',
+        'text' => 'In the exchange office you can exchange from Balance to Purchase Balance. When exchanging you get a %s%% bonus on your Purchase Balance.',
+        'alert' => 'Exchange is possible only in one direction',
+        'give' => 'You give',
+        'receive' => 'You receive',
+        'success' => 'Balance exchanged with success!',
+        'errorBalance' => 'There is not enough funds to exchange!',
+        'errorMin' => 'The minimum amount for the exchange is',
+    ],
+
+    'insert' => [
+        'title' => 'Deposit',
+        'text' => 'Payment and transfer of silver to the balance is made automatically. After replenishment you will be credited with %s.',
+        'receive' => 'You receive',
+        'errorMin' => 'The minimum amount for the exchange is',
+    ],
+
+    'payment' => [
+        'title' => 'Withdraw',
+        'give' => 'You give',
+        'receive' => 'You receive',
+        'latests' => 'Last 10 Payments',
+        'success' => 'Your withdrawal request has been paid with success!',
+        'errorUnkdown' => 'Internal error - report it to the administrator!',
+        'error629' => 'ERROR 629. Inform the administrator about it!',
+        'error630' => 'Error 630. Failed to pay! try later',
+        'error631' => 'Error 631. Failed to pay! try later',
+        'errorWait' => 'You have unprocessed applications. Wait for them to complete.',
+        'errorMax' => 'You specified more than there is in your account',
+        'errorMin' => 'The minimum amount for payment is %s %s!',
+        'errorWallet' => 'Wallet is incorrect! Try again!',
     ],
 ];
